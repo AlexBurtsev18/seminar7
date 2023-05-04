0551d1bf529ccbd0ee90d9b9f960296a293b58ad
«Alex18» <alexburtsev18@yandex.ru>
2023-05-04 22:08:36 +0300

    Сделал задачки к семинару 7

--- Program.cs
+++ Program.cs
@@ -12,17 +12,17 @@
 //     return array;
 // }
 
-void Show2dArray(int[,] array)
+// void Show2dArray(int[,] array)
 
-{
-    for(int i = 0; i < array.GetLength(0); i++)
-    {
-        for(int j = 0; j < array.GetLength(1); j++)
-            Console.Write(array[i,j] + " ");
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-}
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//             Console.Write(array[i,j] + " ");
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
 
 // Task 1. Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aij = i+j.
 // Выведите полученный массив на экран.
@@ -45,23 +45,103 @@ void Show2dArray(int[,] array)
 
 // Task 2. Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
 // void
-void Create2dArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i+=2)
-        for (int j = 0; j < array.GetLength(1); j+=2)
-        {
-            array[i, j] = array[i, j] * array[i, j];
-        }
-}
-
-Console.Write("Input a quantity of rows: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a quantity of columns: ");
-int columns = Convert.ToInt32(Console.ReadLine());
-
-Create2dArray(array);
-Show2dArray(array);
-
 
 // Task 3. Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали
 // (с индексами (0,0); (1;1) и т.д.
+
+
+
+// HOMEWORK. 
+// Task 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// void FillArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i, j] = new Random().Next(-50, 50) / 10.0;
+// }
+
+// void PrintArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             Console.Write(array[i, j] + " ");
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// int ReadInt(string mess)
+// {
+//     Console.Write(mess);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int rows = ReadInt("Введите количество строк: ");
+// int colums = ReadInt("Введите количество столбцов: ");
+// double[,] list = new double[rows, colums];
+// FillArray(list);
+// PrintArray(list);
+
+// Task 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i, j] = new Random().Next(1, 10);
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             Console.Write(array[i, j] + " ");
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// int ReadInt(string mess)
+// {
+//     Console.Write(mess);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int rows = ReadInt("Введите индекс строки: ");
+// int colums = ReadInt("Введите индекс столбца: ");
+// int[,] list = new int[5, 6];
+// FillArray(list);
+// PrintArray(list);
+
+// if (rows < list.GetLength(0) && colums < list.GetLength(1)) Console.WriteLine(list[rows, colums]);
+// else Console.WriteLine($"{rows}{colums} -> такого числа в массиве нет");
+
+// Task 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Random random = new Random();
+// int[,] array = new int[random.Next(1, 10), random.Next(1, 10)];
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array[i, j] = random.Next(1, 10);
+//         Console.Write(array[i, j] + " ");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine("---------------------------");
+// Console.WriteLine(array.GetLength(0));
+// for (int j = 0; j < array.GetLength(1); j++)
+// {
+//     double sum = 0;
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         sum += array[i, j];
+//     }
+//     Console.Write($"{ sum / array.GetLength(0)} ");
+// }
+// Console.ReadLine();
